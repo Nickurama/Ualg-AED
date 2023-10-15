b80a52be3e9a389609a7cf0e529c9835715ae963
Nickurama <rokdoombeast@gmail.com>
2023-10-15 19:02:35 +0100

    FunctionTimer done

--- Exercises_Cap2/Empirical Tests/src/App.java
+++ Exercises_Cap2/Empirical Tests/src/App.java
@@ -1,8 +1,20 @@
+import java.util.Random;
+
 public class App
 {
     public static void main(String[] args) throws Exception
     {
-        System.out.println("Hello, World!");
+        Executable exe = () -> waitOneSec();
+        Generator gen = n -> twoSumGenerateExample(n);
+        FunctionTimer functionTimer = new FunctionTimer(exe);
+        functionTimer.startLogging();
+        functionTimer.getAverageCPU(100);
+    }
+
+    public static void waitOneSec()
+    {
+        double start = System.nanoTime();
+        while (System.nanoTime() - start < 1000000000);
     }
 
     public static int twoSum(int[] a)
@@ -16,5 +28,16 @@ public class App
                     count++;
 
         return count;
+    }
+
+    public static int[] twoSumGenerateExample(int n)
+    {
+        Random r = new Random();
+        int[] examples = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            examples[i] = r.nextInt();
+        }
+        return examples;
     }
 }
--- Exercises_Cap2/Empirical Tests/src/FunctionTimer.java
+++ Exercises_Cap2/Empirical Tests/src/FunctionTimer.java
@@ -3,12 +3,37 @@ import java.lang.management.ThreadMXBean;
 
 public class FunctionTimer
 {
-    private Testable testable;
+    private static final int NANO_TO_MILLI_SCALAR = 1000000;
+    private static final int NANO_TO_SEC_SCALAR = 1000000000;
 
-    public FunctionTimer(Testable testable)
+    private boolean isLogging;
+    private Executable executable;
+
+    public FunctionTimer(Executable executable)
     {
-        this.testable = testable;
+        this.executable = executable;
+        this.isLogging = false;
     }
+
+    public static double toSeconds(long n)
+    {
+        return (double) n / (double) NANO_TO_SEC_SCALAR;
+    }
+
+    public static double toMilliseconds(long n)
+    {
+        return n / NANO_TO_MILLI_SCALAR;
+    }
+
+    public void startLogging()
+    {
+        this.isLogging = true;
+    }
+
+    // public long doubledRatioTest(Generator gen)
+    // {
+    //     int n = 250;
+    // }
 
     public long getAverageCPU(int numOfTests)
     {
@@ -17,18 +42,25 @@ public class FunctionTimer
         long[] allThreadIds = threadMXBean.getAllThreadIds();
 
         for (int i = 0; i < numOfTests; i++)
-            elapsedCPU += getExecutionTime(threadMXBean, allThreadIds);
+        {
+            long executionTime = getExecutionTime(threadMXBean, allThreadIds);
+            elapsedCPU += executionTime;
+            if (isLogging)
+                printTest(i, numOfTests, executionTime);
+        }
         elapsedCPU /= numOfTests;
 
+        if (isLogging)
+            printElapsed(elapsedCPU);
         return elapsedCPU;
     }
 
     private long getExecutionTime(ThreadMXBean threadMXBean, long[] allThreadIds)
     {
-        long startTime, stopTime;
+        long startTime, stopTime; // variable initialization before assignemnt to avoid timing accuracy loss
 
         startTime = getCPUTime(threadMXBean, allThreadIds);
-        testable.run();
+        executable.run();
         stopTime = getCPUTime(threadMXBean, allThreadIds);
 
         return stopTime - startTime;
@@ -36,16 +68,43 @@ public class FunctionTimer
 
     private long getCPUTime(ThreadMXBean threadMXBean, long[] allThreadIds)
     {
-        long CPUTime = 0;
+        long TotalCPUTime = 0;
         for (long thread : allThreadIds)
-            CPUTime += threadMXBean.getThreadCpuTime(thread);
-        return CPUTime;
+        {
+            long cpuTime = threadMXBean.getThreadCpuTime(thread);
+            if (cpuTime > 0)
+                TotalCPUTime += threadMXBean.getThreadCpuTime(thread);
+        }
+
+        return TotalCPUTime;
     }
 
+    private void printTest(int current, int max, long executionTime)
+    {
+        System.out.println("Test " + (current + 1) + "/" + max + " - Time: " +
+            (int) toSeconds(executionTime) + "s / " +
+            (int) toMilliseconds(executionTime) + "ms / " +
+            executionTime + "ns");
+    }
+
+    private void printElapsed(long elapsedCPU)
+    {
+        System.out.println("Test Completed.");
+        System.out.println("Average Time: " +
+            (int) toSeconds(elapsedCPU) + "s / " +
+            (int) toMilliseconds(elapsedCPU) + "ms / " +
+            elapsedCPU + "ns");
+    }
 }
 
 
-interface Testable
+interface Executable
 {
     public void run();
+}
+
+
+interface Generator
+{
+    public Object generate(int n);
 }
