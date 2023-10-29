6538a00baeb9ce49d9d5031a7dd79fbee80a611c
Nickurama <rokdoombeast@gmail.com>
2023-10-29 21:11:01 +0000

    everything working but memory limit exceeded

--- Project_2/src/Main.java
+++ Project_2/src/Main.java
@@ -1,30 +1,81 @@
-import aed.collections.QueueArray;
+import java.util.*;
+import aed.collections.*;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        QueueArray<String> queue = new QueueArray<String>(5);
-        queue.enqueue("1");
-        queue.enqueue("2");
-        queue.dequeue();
-        queue.enqueue("3");
-        queue.enqueue("4");
-        queue.dequeue();
-        queue.enqueue("5");
-        queue.enqueue("6");
-        queue.enqueue("7");
+        StingyList<String> list = new StingyList<String>();
+        list.add("0");
+        list.add("1");
+        list.add("2");
+        //list.print();
+        // StingyList<String> list2 = list.reversed();
+        // System.out.println(list.isEmpty());
+        // list.clear();
+        // System.out.println(list.isEmpty());
+        // try
+        // {
+        //     list.addAt(-1, "X");
+        // } catch (Exception e)
+        // {
 
-        System.out.println("Size: " + queue.size());
-        System.out.println("-----");
-        queue.print();
+        // }
+        // list.addAt(0, "X");
+        // list.print();
+        // list.clear();
+        // Object[] array = list.toArray();
+        // for (int i = 0; i < array.length; i++)
+        //     System.out.println(array[i]);
+        // list.print();
 
-        QueueArray<String> queueCopy = queue.shallowCopy();
+        // list.clear();
+        // Object[] array = list.toArray();
+        // for (int i = 0; i < array.length; i++)
+        //     System.out.println(array[i]);
 
-        System.out.println("Size: " + queueCopy.size());
+        // ArrayList<Integer> al = new ArrayList<Integer>();
+        // Iterator<Integer> it = al.iterator();
+        // Iterator<String> it2 = list.iterator();
 
-        System.out.println("-----");
-        queueCopy.print();
+        // System.out.println("---");
+        // list.print();
+        // System.out.println("---");
+        // list.remove();
+        // list.print();
+        // System.out.println("---");
+        // list.remove();
+        // list.print();
+        // System.out.println("---");
+        // list.remove();
+        // list.print();
+        // System.out.println("---");
+        // list.remove();
 
+        StingyList<Integer> listInts = new StingyList<Integer>();
+        listInts.add(1);
+        listInts.add(2);
+        listInts.add(3);
+        listInts.add(4);
+        listInts.add(5);
+        listInts.add(6);
+        listInts.add(7);
+        listInts.add(8);
+        listInts.add(9);
+        listInts.add(10);
+
+        try
+        {
+            listInts.addAt(11, 0);
+        } catch (Exception e)
+        {
+
+        }
+        listInts.addAt(2, 0);
+        System.out.println("IsEmpty: " + listInts.isEmpty());
+        System.out.println("Size: " + listInts.size());
+        Object[] array = listInts.toArray();
+        for (int i = 0; i < array.length; i++)
+            System.out.println(array[i]);
     }
 }
--- Project_2/src/aed/collections/StingyList.java
+++ Project_2/src/aed/collections/StingyList.java
@@ -2,9 +2,207 @@ package aed.collections;
 
 import java.util.Iterator;
 
-public class StingyList<T> implements Iterable<T> {
+public class StingyList<T> implements Iterable<T>
+{
+    public class StingyListIterator implements Iterator<T>
+    {
+        // An iterator of this class is by definition faulty.
+        // To get the next or the previous nodes from the current node without calculating everything
+        // from scratch, the iterator must "stop" on whatever it is iterating.
+        // If the iterator did not save the previous or the next node, it could not move at all.
+        // Thus if the list changes the previous or the next, then the iterator will give a wrong value.
+        // This cannot be prevented with this implementation of a list
+        AdjacentAddresses adj;
 
-    //representamos null como o long 0L.
+        public StingyListIterator()
+        {
+            adj = new AdjacentAddresses(first);
+        }
+
+        public boolean hasNext()
+        {
+            return adj.hasNext();
+        }
+
+        public T next()
+        {
+            T value = adj.get();
+            adj.next();
+            return value;
+        }
+    }
+    private class AdjacentAddresses
+    {
+        private long previous;
+        private long current;
+        private long next;
+        private boolean isReversed;
+
+        public AdjacentAddresses(long startAddr) throws IndexOutOfBoundsException
+        {
+            this.previous = NULL;
+            this.current = NULL;
+            this.next = startAddr;
+            this.isReversed = startAddr == last;
+            next();
+        }
+
+        public long next()// throws IndexOutOfBoundsException
+        {
+            // if (next == NULL)
+            //     throw new IndexOutOfBoundsException("The next node is null");
+            previous = current;
+            current = next;
+            try
+            {
+                next = getNextAddr(current, previous);
+            } catch (Exception e)
+            {
+                next = NULL;
+            }
+            return current;
+        }
+
+        public boolean hasNext()
+        {
+            return current != NULL;
+        }
+
+        public long previous()// throws IndexOutOfBoundsException
+        {
+            // if (previous == NULL)
+            //     throw new IndexOutOfBoundsException("The previous node is null");
+            next = current;
+            current = previous;
+            try
+            {
+                previous = getNextAddr(current, next);
+            } catch (Exception e)
+            {
+                previous = NULL;
+            }
+
+            return current;
+        }
+
+        public long getAddress()
+        {
+            return this.current;
+        }
+
+        public void reverse()
+        {
+            long cache = previous;
+            previous = next;
+            next = cache;
+
+            isReversed = !isReversed;
+        }
+
+        public T remove() throws IndexOutOfBoundsException
+        {
+            if (current == NULL)
+                throw new IndexOutOfBoundsException("No node at current index");
+
+            T result = get();
+            linkPreviousAndNext();
+            long obsoleteCurrent = current;
+            deleteCurrent();
+            adjust(obsoleteCurrent);
+            size--;
+            return result;
+        }
+
+        private T get()
+        {
+            return UNode.get_item(current);
+        }
+
+        private void linkPreviousAndNext()
+        {
+            if (previous != NULL)
+                updateNodeReference(previous, current, next);
+            if (next != NULL)
+                updateNodeReference(next, current, previous);
+        }
+
+        private void deleteCurrent()
+        {
+            UNode.free_node(current);
+            current = NULL;
+        }
+
+        private void adjust(long obsoleteCurrent) throws IndexOutOfBoundsException
+        {
+            if (next == NULL && previous == NULL) // O O O
+            {
+                updateFirstAndLast(obsoleteCurrent, NULL);
+            } else if (next == NULL) // X O O -> endpoint - update first/last
+            {
+                updateFirstAndLast(obsoleteCurrent, previous);
+                previous();
+            } else if (previous == NULL) // O O X -> endpoint - update first/last
+            {
+                updateFirstAndLast(obsoleteCurrent, next);
+                next();
+            } else // X O X -> O X X
+            {
+                current = previous;
+                next();
+            }
+        }
+
+        private void updateFirstAndLast(long obsoleteCurrent, long newAddr)
+        {
+            if (obsoleteCurrent == first)
+                first = newAddr;
+            if (obsoleteCurrent == last)
+                last = newAddr;
+        }
+
+        public void add(T value)
+        {
+            boolean hasReversed = false;
+            if (this.isReversed) // adding only works from left -> right because of assimetry (adding between previous and current)
+            {
+                hasReversed = true;
+                reverse();
+            }
+            long newAddr = UNode.create_node(value, previous, current);
+            linkPreviousAndCurrentTo(newAddr);
+            updateFirstAndLastForAdd(newAddr);
+            next = current;
+            current = newAddr;
+            size++;
+            if (hasReversed)
+                reverse();
+        }
+
+        private void linkPreviousAndCurrentTo(long addr)
+        {
+            if (previous != NULL)
+                updateNodeReference(previous, current, addr);
+            if (current != NULL)
+                updateNodeReference(current, previous, addr);
+        }
+
+        private void updateFirstAndLastForAdd(long newAddr)
+        {
+            if (previous == NULL) // then current might be first/last
+                updateFirstOrLast(current, newAddr);
+            if (current == NULL) // then previous might be first/last
+                updateFirstOrLast(previous, newAddr);
+        }
+
+        private void updateFirstOrLast(long obsoleteCurrent, long newAddr)
+        {
+            if (obsoleteCurrent == first)
+                first = newAddr;
+            else if (obsoleteCurrent == last)
+                last = newAddr;
+        }
+    }
+
     private static final long NULL = 0L;
 
     private int size;
@@ -13,7 +211,16 @@ public class StingyList<T> implements Iterable<T> {
 
     public StingyList()
     {
-        //TODO: Implement
+        this.size = 0;
+        this.first = NULL;
+        this.last = NULL;
+    }
+
+    public StingyList(long first, long last, int size)
+    {
+        this.size = size;
+        this.first = first;
+        this.last = last;
     }
 
     // Embora não seja obrigatório, aconselho-vos a implementar estes 3 métodos seguintes, pois o código da StingyList pode
@@ -26,114 +233,190 @@ public class StingyList<T> implements Iterable<T> {
     //       from -- to --> Node -- to --> beyond
     //       beyond <-- to -- Node <-- to -- from
     //
-    long getBeyond(long node, long fromAddr)
+    private long getNextAddr(long currentAddr, long previousAddr)
     {
-        //Optional: Implement
-        return 0;
+        return UNode.get_prev_next_addr(currentAddr) ^ previousAddr;
     }
 
     // Atualiza uma das referências do nó (pode ser usado para atualizar o previous ou o next).
     // Recebe como argumento um endereço para o nó, um endereço para a ligação que queremos atualizar (previous ou next),
     // e o novo endereço a usar. Se passármos o previous, este método atualiza apenas o ponteiro para o previous
     // mantendo o ponteiro para o next, e vice-versa.
-    static void updateNodeReference(long node, long oldAddr, long newAddr)
+    private static void updateNodeReference(long nodeAddr, long oldAddr, long newAddr)
     {
-        //Optional: Implement
+        long pointers = UNode.get_prev_next_addr(nodeAddr);
+        long opposite = pointers ^ oldAddr; // gets the opposite (old: next -> previous) (old: previous -> next) 
+        UNode.set_prev_next_addr(nodeAddr, opposite ^ newAddr); // gets a new addres with previous/next + newAddr
     }
 
     //Atualiza ambas as referências do nó em simultâneo (previous e next). Útil quando queremos atualizar ambas,
     //e já temos as referências para o novo previous e o novo next. Recebe como argumentos o novo nó previous para o
     // qual queremos apontar, e o novo nó next para o qual queremos apontar.
-    void updateBothNodeReferences(long node, long prevAddr, long nextAddr)
+    private void updateBothNodeReferences(long nodeAddr, long prevAddr, long nextAddr)
     {
-        //Optional: Implement
+        UNode.set_prev_next_addr(nodeAddr, prevAddr ^ nextAddr);
     }
 
 
     //Stingy List Methods
 
-    public void add(T item)
+    public void add(T item) throws IllegalArgumentException
     {
-        //TODO: Implement
+        if (item == null)
+            throw new IllegalArgumentException("Argument is null");
+
+        long nodeAddr;
+        if (size == 0)
+        {
+            nodeAddr = UNode.create_node(item, NULL, NULL);
+            first = nodeAddr;
+        } else
+        {
+            nodeAddr = UNode.create_node(item, last, NULL);
+            updateNodeReference(last, NULL, nodeAddr);
+        }
+        last = nodeAddr;
+        this.size++;
     }
 
-    public T remove()
+    public T remove() throws IndexOutOfBoundsException
     {
-        //TODO: Implement
-        //DO NOT FORGET to FREE memory
-        return null;
+        try
+        {
+            return removeAt(this.size - 1);
+        } catch (IndexOutOfBoundsException e)
+        {
+            throw new IndexOutOfBoundsException("List is empty: " + e.getMessage());
+        }
     }
 
-    public T get()
+    public T get() throws IndexOutOfBoundsException
     {
-        //TODO: Implement
-        return null;
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("List is empty.");
+        return get(size - 1);
     }
 
-    public T get(int i)
+    public T get(int i) throws IndexOutOfBoundsException
     {
-        //TODO: Implement
-        return null;
+        throwIfInvalidIndex(i);
+        return getAdjacentAddresses(i).get();
     }
 
-    public T getSlow(int i)
+    private AdjacentAddresses getAdjacentAddresses(int i) throws IndexOutOfBoundsException
     {
-        //TODO: Implement
-        return null;
+        AdjacentAddresses adjacent = setupAdressesFromIndex(i);
+        int iterations = getIterationsFromIndex(i);
+
+        while (iterations-- > 0)
+            adjacent.next();
+
+        return adjacent;
     }
 
-    public void addAt(int i, T item)
+    private AdjacentAddresses setupAdressesFromIndex(int i) throws IndexOutOfBoundsException
     {
-        //TODO: Implement
+        return isOnUpperHalf(i) ? new AdjacentAddresses(last) : new AdjacentAddresses(first);
     }
 
-    public T removeAt(int i)
+    private boolean isOnUpperHalf(int i)
     {
-        //TODO: Implement
-        //DO NOT FORGET to FREE memory
-        return null;
+        return i + 1 > this.size / 2;
+    }
+
+    private int getIterationsFromIndex(int i)
+    {
+        return isOnUpperHalf(i) ? this.size - (i + 1) : i;
+    }
+
+    private void throwIfInvalidIndex(int i) throws IndexOutOfBoundsException
+    {
+        if (i < 0 || i >= this.size)
+            throw new IndexOutOfBoundsException("Index " + i + " out of range: " + (this.size - 1));
+    }
+
+    public T getSlow(int i) throws IndexOutOfBoundsException
+    {
+        throwIfInvalidIndex(i);
+
+        AdjacentAddresses adjacent = new AdjacentAddresses(first);
+        while (i-- > 0)
+            adjacent.next();
+        return adjacent.get();
+    }
+
+    public void addAt(int i, T item) throws IndexOutOfBoundsException
+    {
+        throwIfInvalidIndex(i);
+        AdjacentAddresses a = getAdjacentAddresses(i);
+        a.add(item);
+    }
+
+    public T removeAt(int i) throws IndexOutOfBoundsException
+    {
+        throwIfInvalidIndex(i);
+        try
+        {
+            return getAdjacentAddresses(i).remove();
+        } catch (IndexOutOfBoundsException e)
+        {
+            throw new IndexOutOfBoundsException(e.getMessage());
+        }
     }
 
     public void reverse()
     {
-        //TODO: Implement
+        long cache = this.first;
+        this.first = this.last;
+        this.last = cache;
     }
 
     public StingyList<T> reversed()
     {
-        //TODO: Implement
-        return null;
+        return new StingyList<T>(this.last, this.first, this.size);
     }
 
     public void clear()
     {
-        //TODO: Implement
-        //DO NOT FORGET to FREE memory
-        return null;
+        if (this.size == 0)
+            return;
+
+        AdjacentAddresses adjacent = new AdjacentAddresses(this.first);
+        while (adjacent.hasNext())
+            adjacent.remove();
     }
 
     public boolean isEmpty()
     {
-        //TODO: Implement
-        return false;
+        return this.size == 0;
     }
 
-    public int Size()
+    public int size()
     {
-        //TODO: Implement
-        return 0;
+        return this.size;
     }
 
     public Object[] toArray()
     {
-        //TODO: Implement
-        return null;
+        Object[] result = new Object[this.size];
+        if (this.size == 0)
+            return result;
+
+        Iterator<T> it = iterator();
+        for (int i = 0; i < this.size; i++)
+            result[i] = it.next();
+        return result;
     }
 
     @Override
     public Iterator<T> iterator()
     {
-        //TODO: Implement
-        return null;
+        return new StingyListIterator();
+    }
+
+    public void print()
+    {
+        for (T item : this)
+            System.out.println(item);
     }
 }
