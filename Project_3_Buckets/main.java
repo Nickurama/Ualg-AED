776adb9b71b3cc0b18733f04b7547c0c3604e3a7
Nickurama <rokdoombeast@gmail.com>
2023-11-29 22:58:48 +0000

    Cordel done

--- Project_3_Buckets/main.java
+++ Project_3_Buckets/main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import aed.collections.Cordel;
 import aed.sorting.RecursiveStringSort;
 import aed.sorting.Sort;
 
@@ -6,41 +7,47 @@ public class main
 {
     public static void main(String[] args)
     {
-        //String[] strsArray = {"Hello", "String2", "aHello", "Space Test"};
-        //String[] strsArray = {"b", "C", "B", "c", "D", "A", "a", "aae", "aac", "aag", "aad", "aaab", "aaa", "aaaa", "aaf", "aab", "aaa"};
-        // String[] strsArray = {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa", "aaaaaaaaaaa", "aaaaaaaaaaaa", "aaaaaaaaaaaaa",
-        //         "aaaaaaaaaaaaaa", "aaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaa"};
-        //String[] strsArray = {"\0", "ʜǚƇο˝ƊŢ>μȴƑʮƄǁœŹ", "\"͎ȔȽǍûʃ", "\"Σɼ͒ˢǵ΃ǫ±̆įʠʊȡ̐¸ơ", "&ǮȌǠ!ήǑǟ"};
-        //String[] strsArray = {"\0", "ʜǚƇο˝ƊŢ>μȴƑʮƄǁœŹ", "\"͎ȔȽǍûʃ", "\"Σɼ͒ˢǵ΃ǫ±̆įʠʊȡ̐¸ơ", "&ǮȌǠ!ήǑǟ", Character.toString(127),
-        //        " ", "  ", Character.toString(0)};
-        //String[] strsArray = {"Zeus", "alameda", "alfredo", "chouriço", "joão", "maria", "ola", "ola", "teste", "único"};
-        //RecursiveStringSort.insertionSort(Arrays.asList(strsArray));
-        //RecursiveStringSort.fasterSort2(strsArray);
-        String[] strsArray = generate(1000);
-        String[] strs0 = strsArray.clone();
-        String[] strs1 = strsArray.clone();
-        RecursiveStringSort.fasterSortEnglish(strs0);
-        RecursiveStringSort.fasterSort2(strs1);
-        // for (String s : strsArray)
+        Cordel cord0 = new Cordel("Ola, ");
+        Cordel cord1 = new Cordel("isto e");
+        Cordel cord2 = new Cordel(" um ");
+        Cordel cord3 = new Cordel("cor");
+        Cordel cord4 = new Cordel("del!");
+
+        cord0 = cord0.append("isto e").append(" um ").append("cor").append("del!");
+        //cord0.println();
+
+        //System.out.println(cord0.length());
+
+        cord0 = new Cordel("Ola, ");
+
+        Cordel layer2_1 = new Cordel(cord1, cord2);
+        Cordel layer1_0 = new Cordel(cord0, layer2_1);
+        Cordel layer1_1 = new Cordel(cord3, cord4);
+        Cordel layer0 = new Cordel(layer1_0, layer1_1);
+        // layer0.println();
+        // layer0.printInfo();
+
+        // for (int i = 0; i < layer0.length(); i++)
+        //     System.out.print(layer0.charAt(i));
+
+        // Cordel[] splitTest = layer0.split(22);
+        // splitTest[0].println();
+        // splitTest[1].println();
+
+        // System.out.println(layer0.length());
+        // String cocat = "Ola, " + "isto e" + " um " + "cor" + "del!";
+        // System.out.println(cocat.length());
+
+        // layer0.println();
+        // layer0 = layer0.delete(0, 22);
+        // layer0.println();
+
+        // for (String s : layer0)
         //     System.out.println(s);
-        System.out.println("recursive isSorted: " + Sort.isSorted(strs0));
-        System.out.println("faster isSorted: " + Sort.isSorted(strs1));
-    }
 
-    private static String[] generate(Integer size)
-    {
-        Random R = new Random();
-        final int maxLen = 1000;
-
-        String[] strings = new String[size];
-        for (int i = 0; i < size; i++)
-        {
-            int len = 1 + R.nextInt(maxLen);
-            StringBuilder buffer = new StringBuilder(len);
-            for (int j = 0; j < len; j++)
-                buffer.append((char) R.nextInt(1000));
-            strings[i] = buffer.toString();
-        }
-        return strings;
+        layer0.println();
+        System.out.print("\"");
+        layer0.print(15, 21);
+        System.out.print("\"");
     }
 }
--- Project_3_Buckets/src/aed/collections/Cordel.java
+++ Project_3_Buckets/src/aed/collections/Cordel.java
@@ -3,163 +3,207 @@ package aed.collections;
 import java.util.Iterator;
 import java.util.Stack;
 
-public class Cordel implements Iterable<String>{
+public class Cordel implements Iterable<String>
+{
+    private class CordelIterator implements Iterator<String>
+    {
+        Stack<Cordel> state;
 
-    //Nesta implementação, iremos usar a própria classe Cordel como nó, em vez de termos uma classe separada para representar
-    //os nós.
-    //Há algumas razões para fazer isto, a mais importante é que pretendemos fazer uma implementação imutável (não destrutiva)
-    //de cordeis, o que quer dizer que uma vez criado um cordel, os seus campos não vão mais ser alterados, e portanto
-    //quer dizer que parte dos nossos métodos vão ter que retornar cordéis, pois é a única forma de conseguirmos construir
-    //cordéis mais elaborados. Por exemplo, quando quero concatenar uma string a um cordel, vou ter que retornar um
-    // novo cordel (não posso alterar o cordel original) que corresponde a um nó de concatenação entre o cordel original e um
-    // novo cordel folha com a string recebida.
+        public CordelIterator()
+        {
+            state = new Stack<Cordel>();
+            state.push(Cordel.this);
+        }
 
-    //Devido a esta característica, e ao facto de toda a informação que precisamos estar guardada no nó, não
-    //se justifica estar a criar uma classe interna Node, e fazer com que a Classe cordel tenha apenas um membro root
-    // (iríamos gastar mais memória sem nenhuma vantagem).
+        public boolean hasNext()
+        {
+            return !state.empty();
+        }
 
-    //Estes membros são declarados como final, para o compilador de Java garantir que uma vez definidos no construtor
-    //nunca mais podem ser alterados.
+        public String next()
+        {
+            Cordel current = state.pop();
+
+            if (current.isLeaf())
+                return current.string;
+
+            state.push(current.right);
+            state.push(current.left);
+            return next();
+        }
+    }
+
     private final Cordel left;
     private final Cordel right;
-    //Importante: normalmente, os cordéis implementam-se usando apenas informação sobre o tamanho da subárvore esquerda
     private final int leftLength;
-    //Mas no nosso caso, queremos também guardar informação sobre o tamanho da subárvore direita para tornar a contatenação
-    //ligeiramente mais eficiente (se não tivessemos este membro, teríamos de pagar O(log n) para calcular o tamanho do
-    //lado direito)
     private final int rightLength;
     private final String string;
 
-    //Cria e inicializa um cordel folha (LEAF) com a string s recebida
-    //A string s não pode ser nula
     public Cordel(String s)
     {
-        //todos os membros têm de ser inicializados, dado que foram declarados como final
-        //lembrem-se que um nó folha não tem filhos.
-        //TODO: implement
+        if (s == null)
+            throw new IllegalArgumentException("Can't create leaf with null string.");
+
+        this.left = null;
+        this.right = null;
+        this.leftLength = s.length(); // saves some efficiency off length()
+        this.rightLength = 0;
+        this.string = s;
     }
 
-    //Cria e inicializa um cordel que corresponde a um nó de concatenação (CONCAT)
-    //e que concatena os dois cordéis recebidos
     public Cordel(Cordel left, Cordel right)
     {
-        //TODO: implement
+        this.left = left;
+        this.right = right;
+        this.leftLength = left.length();
+        this.rightLength = right.length();
+        this.string = null;
     }
 
-    //Retorna o tamanho de um cordel, ou seja, o número de caracteres total guardado no cordel.
+    private boolean isLeaf()
+    {
+        return this.string != null;
+    }
+
     public int length()
     {
-        //TODO: implement
-		return 0;
+        return this.leftLength + this.rightLength;
     }
 
-    //Dado uma string, retorna um novo cordel que corresponde à concatenação à direita da string recebida com o cordel.
     public Cordel append(String s)
     {
-        //TODO: implement
-		return null;
+        return this.append(new Cordel(s));
     }
 
-    //Dado um Cordel c, retorna um novo cordel que corresponde à concatenação à direita do cordel c recebido com o cordel.
     public Cordel append(Cordel c)
     {
-		//TODO: implement
-        return null;
+        return new Cordel(this, c);
     }
 
-    //Dado uma string, retorna um novo cordel que corresponde à concatenação à esquerda da string recebida com o cordel.
     public Cordel prepend(String s)
     {
-        //TODO: implement
-		return null;
+        return this.prepend(new Cordel(s));
     }
 
-    //Dado um Cordel c, retorna um novo cordel que corresponde à concatenação à esquerda do cordel c recebido com o cordel.
     public Cordel prepend(Cordel c)
     {
-		//TODO: implement
-        return null;
+        return new Cordel(c, this);
     }
 
-    //Imprime as strings guardadas dentro deste cordel, pela ordem da string mais à esquerda para a mais à direita.
-    //Não imprime mudanças de linha.
     public void print()
     {
-        //TODO: implement
+        if (this.isLeaf())
+        {
+            System.out.print(this.string);
+            return;
+        }
+
+        this.left.print();
+        this.right.print();
     }
 
-    //Igual ao anterior, mas imprime uma nova linha no fim
     public void println()
     {
         print();
         System.out.println();
     }
 
-    //Imprime informação sobre cada um dos nós da árvore.
     public void printInfo()
     {
-        //TODO: implement
+        if (this.isLeaf())
+        {
+            System.out.print("LEAF:\"" + this.string + "\"");
+            return;
+        }
+
+        System.out.print("CONCAT[" + leftLength + "," + rightLength + "]:{");
+        this.left.printInfo();
+        System.out.print(",");
+        this.right.printInfo();
+        System.out.print("}");
+
     }
 
-    //igual ao anterior, mas imprime uma nova linha no fim
     public void printInfoNL()
     {
         this.printInfo();
         System.out.println();
     }
 
-    //Dado um índice, devolve o caracter correspondente ao índice i do cordel.
     public char charAt(int i)
     {
-        //TODO: implement
-		return 0;
+        if (this.isLeaf())
+            return this.string.charAt(i);
+
+        if (i >= this.leftLength)
+            return this.right.charAt(i - this.leftLength);
+        return this.left.charAt(i);
     }
 
-    //Dado um índice, parte o cordel em dois cordéis no índice i. É devolvido um array com 2 cordéis.
-    // O primeiro cordel contém as strings com todos os caracteres desde o índice 0 até o índice i-1,
-    // e o segundo cordel contém as strings com todos os caracteres desde o índice i até ao fim.
     public Cordel[] split(int i)
     {
-        //TODO: implement
-		return null;
+        if (this.isLeaf())
+            return new Cordel[] {new Cordel(string.substring(0, i)), new Cordel(string.substring(i))};
+
+        if (this.leftLength == i)
+            return new Cordel[] {this.left, this.right};
+
+        if (this.leftLength > i)
+        {
+            Cordel[] leftSplit = this.left.split(i);
+            leftSplit[1] = leftSplit[1].append(this.right);
+            return leftSplit;
+        }
+
+        Cordel[] rightSplit = this.right.split(i - this.leftLength);
+        rightSplit[0] = rightSplit[0].prepend(this.left);
+        return rightSplit;
     }
 
-    //Dado um índice, e uma string que não pode ser nula, retorna um cordel que corresponde ao
-    // resultado de inserirmos a string s, na posição i do cordel.
     public Cordel insertAt(int i, String s)
     {
-        //TODO: implement
-		return null;
+        if (i == 0)
+            return this.prepend(s);
+
+        if (i == this.length())
+            return this.append(s);
+
+        Cordel[] split = this.split(i);
+        return split[0].append(new Cordel(s)).append(split[1]);
     }
 
-    //Dado um índice i, e um tamanho, apaga a partir do índice i do cordel
-    // um número de caracteres correspondentes ao tamanho recebido.
     public Cordel delete(int i, int length)
     {
-       //TODO: implement
-	   return null;
+        if (i == 0)
+            return this.split(length)[1];
+
+        Cordel left = this.split(i)[0];
+        Cordel right = this.split(i + length)[1];
+        return left.append(right);
     }
 
-    
-
-    //Devolve um iterador que vai percorrer todas as strings guardadas em nós folha deste cordel,
-    // pela ordem correta (i.e. da esquerda para a direita).
     @Override
-    public Iterator<String> iterator() {
-        //TODO: implement
-		return null;
+    public Iterator<String> iterator()
+    {
+        return new CordelIterator();
     }
 
-    //Dado um índice i, e um tamanho, imprime um número de caracteres do cordel igual ao tamanho recebido,
-    // a partir do índice i (inclusive).
     public void print(int i, int length)
     {
-        //TODO: implement
+        if (this.isLeaf())
+            System.out.print(this.string.substring(i, Math.min(i + length, this.string.length())));
+        else if (i < this.leftLength)
+        {
+            this.left.print(i, length);
+            if (i + length >= this.leftLength)
+                this.right.print(0, length - (this.leftLength - i));
+        } else
+            this.right.print(i - this.leftLength, length);
     }
 
     public void println(int i, int length)
     {
-        print(i,length);
+        print(i, length);
         System.out.println();
     }
 
